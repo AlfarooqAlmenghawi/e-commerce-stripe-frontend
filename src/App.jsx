5e5ae2053b747703e7420df5d65f130d6d7fb512
Alfarooq Almenghawi <a.almenghawi123@gmail.com>
2025-06-29 19:06:16 +0100

    initialise with background image

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import MainPage from "./components/MainPage/MainPage.jsx";
+
+function App() {
+  return (
+    <>
+      <MainPage />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/MainPage/MainPage.jsx
@@ -0,0 +1,12 @@
+import "./MainPage.css";
+
+const MainPage = () => {
+  return (
+    <div className="main-page">
+      <h1>Welcome to the Main Page</h1>
+      <p>This is the main content area of the application.</p>
+    </div>
+  );
+};
+
+export default MainPage;
